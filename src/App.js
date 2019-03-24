aa2a811987ff6051e11c31831b62f0f8005e1e39
Fil Luzadas <flg.l@outlook.com>
2019-03-24 13:20:58 -0400

    Implemented basic CAD to USD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    initialCurrency: 0,
+    convertedCurrency: 0,
+    curEx: {},
+  };
+
+  async componentDidMount() {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    try {
+      const res = await fetch(
+        `https://free.currencyconverterapi.com/api/v6/convert?apiKey=${API_KEY}&q=CAD_USD,USD_CAD&compact=y`,
+      );
+      const conversion = await res.json();
+      this.setState({ curEx: conversion });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onChange = (e) => {
+    if (e.target.value === null) {
+      this.setState({ initialCurrency: 0, convertedCurrency: 0 });
+    } else {
+      this.setState({ initialCurrency: parseInt(e.target.value) }, () => {
+        this.convert();
+      }); // use callback function to immediately use new state
+    }
+  };
+
+  convert = () => {
+    const { initialCurrency, curEx } = this.state;
+    const result = +(initialCurrency * curEx.CAD_USD.val).toFixed(2);
+    this.setState({ convertedCurrency: result });
+  };
+
   render() {
+    const { convertedCurrency } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Currency Converter</h1>
+        <input onChange={e => this.onChange(e)} type="text" />
+        {' '}
+CAD =
+        <input readOnly type="text" value={convertedCurrency} />
+        {' '}
+USD
       </div>
     );
   }
